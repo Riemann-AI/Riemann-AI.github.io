a8c083ca451885a6058ad92b67332d2a0ab6abe9
Amedeo Ebolese <73333524+Riemann-AI@users.noreply.github.com>
2020-12-28 15:38:15 +0100

    changed project

---
+++ data.js
@@ -0,0 +1,56 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano_20201125_163543_0_mod",
+      "name": "PANO_20201125_163543_0_MOD",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.08961169337878339,
+        "pitch": 0.11353219927761593,
+        "fov": 1.2812361540040014
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04705872523501142,
+          "pitch": 0,
+          "rotation": 4.71238898038469,
+          "target": "0-pano_20201125_163543_0_mod"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6039578308431501,
+          "pitch": 0.10786920897873742,
+          "title": "cioa",
+          "text": "hello world"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
